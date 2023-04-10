7c339e29e20262d03b9009d38548ab146336912e
saifrahmn <saifrahmn78@gmail.com>
2023-04-11 03:17:18 +0530

    mikro-orm is all setup

---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,20 @@
+import { Post } from "../entities/Post";
+import { __prod__ } from "./constants";
+import { Options } from '@mikro-orm/core';
+import path from "path";
+
+const config: Options = {
+    migrations:{
+        path: path.join(__dirname,'./migrations'),
+        pathTs: undefined, 
+        glob: '!(*.d).{js,ts}'
+    },
+        entities: [Post],
+        dbName: 'lireddit',
+        user: 'postgres',
+        password: 'root',
+        type: 'postgresql',
+        debug: !__prod__,
+    
+};
+export default config;
